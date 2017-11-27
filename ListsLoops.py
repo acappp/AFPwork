273bebcdc5c148aacb447816de4d9199fdfa4952
Capp <19acappellucci@mlmc.vic.edu.au>
2017-11-27 14:02:01 +1100

    Learning List Loops

--- ListsLoops.py
+++ ListsLoops.py
@@ -7,3 +7,8 @@ points = []
 gamePoints = input('Enter the points for each game played, E.G 5,6,7,8')
 points = gamePoints.split(',')
 print(points)
+
+total = 0
+for x in points:
+    total += int(x)
+    print(total)
